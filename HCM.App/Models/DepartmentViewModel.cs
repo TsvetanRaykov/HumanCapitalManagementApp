a1c45f68bbc9e8f479dbe45164a322283941d346
Tsvetan Raykov <tsvetan.raykov@gmail.com>
2023-03-06 17:08:27 +0200

    feat: HCM.App/Add Departments CRUD UI

---
+++ HCM.App/Models/DepartmentViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HCM.App.Models;
+
+public class DepartmentViewModel
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(50, MinimumLength = 3, ErrorMessage = "Department Name must be at least 3 characters long.")]
+    public string Name { get; set; }
+
+    [Required]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "Department Address must be at least 3 characters long.")]
+    public string Address { get; set; }
+
+    public string? Description { get; set; }
+}
---
+++ HCM.App/Models/Mapping/DepartmentProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using HCM.Shared.Data.DTO;
+
+namespace HCM.App.Models.Mapping;
+
+public class DepartmentProfile : Profile
+{
+    public DepartmentProfile()
+    {
+        CreateMap<DepartmentViewModel, DepartmentDto>();
+        CreateMap<DepartmentDto, DepartmentViewModel>();
+    }
+}
